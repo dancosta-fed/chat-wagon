7b34fd76b828324449dabd580a5d009def7e3fca
Daniel Costa <dancosta.id@gmail.com>
2022-03-13 11:17:52 -0300

    Created the firebase doc on the firebase website. Also, enabled the google sign in and the database

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,4 +1,22 @@
 import React from "react";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks-auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp ({
+  apiKey: "AIzaSyDvNHhvlL6y-ZQx84tRq_hvWr8eu-hK7lE",
+  authDomain: "chatwagon-900ab.firebaseapp.com",
+  projectId: "chatwagon-900ab",
+  storageBucket: "chatwagon-900ab.appspot.com",
+  messagingSenderId: "534679726932",
+  appId: "1:534679726932:web:46ce8f91046c9a9ac2b6c1",
+  measurementId: "G-XVZSJVLYXS"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.Firestore();
 
 class App extends React.Component {
   render() {
